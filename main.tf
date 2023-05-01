ac0dff99a8b6a8ad727d1bab8837c7e6329f6106
gantasure6786 <gantasure6786@gmail.com>
2023-05-01 12:05:58 +0530

    updated main.tf

--- main.tf
+++ main.tf
@@ -11,18 +11,6 @@ resource "aws_instance" "web" {
   instance_type               = "t3.micro"
   associate_public_ip_address = true
 
-  user_data = <<-EOF
-              #!/bin/bash
-              curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
-              /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-              echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-              https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-              /etc/apt/sources.list.d/jenkins.list > /dev/null
-              sudo apt-get update
-              sudo apt-get install fontconfig openjdk-11-jre
-              sudo apt-get install jenkins
-              EOF
-
   tags = {
     Name = "HelloWorld"
   }
@@ -31,18 +19,6 @@ resource "aws_instance" "example" {
   ami                         = "ami-007855ac798b5175e"
   instance_type               = "t2.micro"
   associate_public_ip_address = true
-
-  user_data = <<-EOF
-              #!/bin/bash
-              curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
-              /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-              echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-              https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-              /etc/apt/sources.list.d/jenkins.list > /dev/null
-              sudo apt-get update
-              sudo apt-get install fontconfig openjdk-11-jre
-              sudo apt-get install jenkins
-              EOF
 
   # Replace with your desired key pair name
   key_name = "my-keypair"
